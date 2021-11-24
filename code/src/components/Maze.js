91aa64c6f7b82d30578c3f122f117d7a8ef38fbc
intehon <emelie.soderhielm@gmail.com>
2021-11-24 20:01:34 +0100

    added images and styled components

--- code/src/components/Maze.js
+++ code/src/components/Maze.js
@@ -3,20 +3,48 @@ import { useSelector, useDispatch } from 'react-redux'
 import { nextStep } from '../reducers/game'
 import {End } from './End'
 import styled from 'styled-components'
+import 0,1 from '../images/0,1.jpg'
 
 const GameBoard = styled.section`
-    background-image: url(${(props) => props.url});
-
+  background-image: url(img-${props.coordinates});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
 `
 
 const Content = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    /* height: 100vh; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* flex-wrap: wrap; */
+  align-items: center;
+  justify-content: space-evenly;
+  /* width: 100px; */
+  min-height: 300px;
+  border: whitesmoke solid 4px;
+  border-radius: 6px;
+  padding: 20px;
+  background: rgba(0, 0, 0, 0.6);
+  color: #f7f5e1;
+
+  @media (min-width: 768px) {
+    min-width: 600px;
+    padding: 30px;
+  }
+`
+
+const TextContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
 export const Maze = () => {
     const { description, coordinates, actions } = useSelector(store => store.game.currentPosition)
@@ -28,7 +56,7 @@ export const Maze = () => {
     }
 
     const ActionCard = ({ description, type, direction }) => (
-        <div className='action-card'>
+        <div>
             <p>{description}</p>
             <button onClick={() => handleButtonClick(type, direction)}>
                 {type} {direction.toLowerCase()}
@@ -39,14 +67,16 @@ export const Maze = () => {
     return (
         <GameBoard>
             <Content>
-                {actions.length !== 0 && 
-                <div>
-                    <h1>Current description: {description}</h1>
-                    <p>Your current position: {coordinates}</p>
-                </div>
-                } 
-                {actions.length === 0 ? <End /> :
-                actions.length > 0 && actions.map(item => <ActionCard key={item.direction} {...item} />)}
+                <Container>
+                    {actions.length !== 0 && 
+                    <TextContent>
+                        <h1>Current description: {description}</h1>
+                        <p>Your current position: {coordinates}</p>
+                    </TextContent>
+                    } 
+                    {actions.length === 0 ? <End /> :
+                    actions.length > 0 && actions.map(item => <ActionCard key={item.direction} {...item} />)}
+                </Container>
             </Content>
         </GameBoard>
     )
--- code/src/components/Start.js
+++ code/src/components/Start.js
@@ -3,6 +3,60 @@ import { useDispatch } from "react-redux"
 import { game } from '../reducers/game'
 import { startGame } from "../reducers/game"
 import Button from '@mui/material/Button'
+import styled from 'styled-components'
+import maze from '../images/maze.jpg'
+
+
+
+const GameBoard = styled.section`
+  background-image: url(${(maze)});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+`
+
+const Content = styled.div`
+display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* flex-wrap: wrap; */
+  align-items: center;
+  justify-content: space-evenly;
+  /* width: 100px; */
+  min-height: 300px;
+  border: whitesmoke solid 4px;
+  border-radius: 6px;
+  padding: 20px;
+  background: rgba(0, 0, 0, 0.6);
+  color: #f7f5e1;
+
+  @media (min-width: 768px) {
+    min-width: 600px;
+    padding: 30px;
+  }
+`
+
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+`
+const Label = styled.label`
+display: flex;
+flex-direction: column;
+padding-bottom: 20px;
+`
+const TextContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
 export const Start = () => {
   const dispatch = useDispatch()
@@ -19,18 +73,26 @@ export const Start = () => {
 
   return (
     <>
-        <h1>Welcome to the maze!</h1>
-        <form onSubmit={handleSubmit}>
-        <label>
-          Enter your name to get started
-          <input type='text' required onChange={handleInputChange} />
-        </label>
-        <Button
-          variant="contained"
-          type='submit'>
-            Enter the labyrinth
-            </Button>
-      </form>
+      <GameBoard>
+          <Content>
+            <Container>
+              <h1>Welcome to the maze!</h1>
+                <TextContent>
+                  <Form onSubmit={handleSubmit}>
+                    <Label>
+                      <p>Enter your name to get started</p>
+                      <input type='text' required onChange={handleInputChange} />
+                    </Label>
+                    <Button
+                      variant="contained"
+                      type='submit'>
+                        Enter the labyrinth
+                    </Button>
+                </Form>
+            </TextContent>
+          </Container>
+        </Content>
+      </GameBoard>
     </>
   )
 }
